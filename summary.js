2dd196a43de8d2ac094d77d180fceec66d55234e
Meanwhile <handyarifincode80@gmail.com>
2025-04-01 15:01:25 +0700

    ini hannya summary

---
+++ summary.js
@@ -0,0 +1,4 @@
+//*
+//cara inisialisasi atau pembuatan repostiory local harus berada di folder yang ingin dibuat
+//cara masukan file ke dalam stagin area atau area sementara adalah dengan git add
+//cara memastikan perubahan yang terjadi pada file ke record harus plus save data changes setelah itu kasih commit dan bisa langusng ke git push origin main
