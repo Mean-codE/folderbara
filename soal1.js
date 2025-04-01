9cb2eca3c50e6c0c2eabdc5d9cb8f0be4d04983a
Meanwhile <handyarifincode80@gmail.com>
2025-04-01 11:53:44 +0700

    menambahkan variabel result 2 hingga 4

---
+++ soal1.js
@@ -0,0 +1,34 @@
+/**
+ * 1. Terdapat array dengan nilai sebagai berikut
+ [ 19 , 7 , 24 , 1 , 76 , 3 , 9 , 7 ]
+ * * 1. Tentukan jika nilai target terdapat pada array atau tidak
+ * 2. Jika terdapat pada array , kembalikan index array tersebut
+ * 3. Jika tidak terdapat pada array , kembalikan -1
+ * * Nilai Target
+ * a. 7
+ * b. 33
+ */
+
+const arrayExample = [19, 7, 24, 1, 76, 3, 9, 7];
+
+const linearSearchAlgorithm = (arrayData, targetValue) => {
+  // for (let i = 0; i < arrayData.length; i++) {
+  //   if (arrayData[i] === targetValue) {
+  //     return i;
+  //   }
+  // }
+  // return -1;
+
+  return (returnWithMethid = arrayData.findIndex((element) => {
+    return element === targetValue;
+  }));
+};
+
+const result = linearSearchAlgorithm(arrayExample, 7);
+console.log(result);
+
+const result2 = "wow";
+
+const result3 = "wow2";
+
+const result4 = "wow3";
